594d1b7a2735cd39288282ab773f6acef3568b97
LanceJernigan <lance@lancejernigan.com>
2025-08-09 03:59:25 -0400

    UX tidy

--- app/account/library/page.tsx
+++ app/account/library/page.tsx
@@ -5,15 +5,17 @@ import getLibraryQuery from "@/queries/library/getLibrary";
 import { Game } from "@/app/api/graphql/resolvers/library/types";
 import GamesGrid from "@/components/gamesGrid";
 import styles from "./page.module.css";
+import { useMemo } from "react";
 
 export default function Library() {
     const { data }: { data: { library: Game[] } } = useSuspenseQuery(getLibraryQuery);
-
-    console.log(data);
+    const games = useMemo(() => {
+        return [...(data?.library || [])].sort((a, b) => a.name.localeCompare(b.name));
+    }, [data.library]);
 
     return (
         <div className={styles.wrapper}>
-            <GamesGrid title="Library" games={data.library} className={styles.grid} />
+            <GamesGrid title="Library" games={games} className={styles.grid} />
         </div>
     )
 }
