0a493f420d7f96ddc9cbaf85c0b24aaea188c6df
LanceJernigan <lance@lancejernigan.com>
2025-08-23 17:23:57 -0400

    Mobile cleanup

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,12 +1,33 @@
 import Menu from "@/components/menu";
 import ProfileOverview from "@/components/profileOverview";
 import styles from './header.module.css';
+import MenuIcon from "@/icons/menu";
+import { useState } from "react";
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
-        <header className={styles.header}>
-            <ProfileOverview />
-            <Menu />
+        <header
+            className={styles.header}
+            data-menuopen={menuOpen}
+        >
+            <div className={styles.top}>
+                <ProfileOverview />
+                <button
+                    className={styles.menuButton}
+                    onClick={toggleMenu}
+                >
+                    <MenuIcon />
+                </button>
+            </div>
+            <div className={styles.menuWrapper}>
+                <Menu />
+            </div>
         </header>
     )
 }
---
+++ icons/menu.tsx
@@ -0,0 +1,17 @@
+const Menu = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={31}
+        height={24}
+        fill="none"
+    >
+        <path
+            stroke="#fff"
+            strokeLinecap="round"
+            strokeWidth={3}
+            d="M2 22h27M2 12h27M2 2h27"
+        />
+    </svg>
+);
+
+export default Menu;
