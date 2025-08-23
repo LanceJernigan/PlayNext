ab6c8322f9ff126afff1fd2be9ad2d59a8294602
LanceJernigan <lance@lancejernigan.com>
2025-08-23 18:24:26 -0400

    Remove text from login

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -40,9 +40,9 @@ export default function Login({ handleSubmit, handleChange, steamId, user, loadi
                 </div>
                 <h1>{user ? user.personaname : "PlayNext"}</h1>
             </header>
-            <p className={styles.content}>
+            {/* <p className={styles.content}>
                 Tired of scrolling through endless game lists? Our tool analyzes your gaming library to recommend titles tailored to your tastes. Whether you&apos;re into fast-paced shooters, cozy farming sims, or story-rich adventures — we&apos;ve got the perfect next game for you. Just connect your library and let the discovery begin!
-            </p>
+            </p> */}
             <form className={styles.form} onSubmit={handleLocalSubmit}>
                 <label className={`${styles.label} ${(loading || user) && styles.labelLoading}`}>
                     <p>Steam ID or Profile URL</p>
